ddaf126757ac1d2da064aa762b9f8e95c9a1c943
darshan527 <darshan.hosamani6@gmail.com>
2021-11-13 14:10:14 +0530

    Auth Require tested

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("home", views.home)]
+urlpatterns = [path("home", views.home), path("restricted", views.restricted)]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 # Create your views here.
 def home(request):
     return render(request, "home/welcome.html", {"today": datetime.today()})
+
+
+# Restricted to logged in users
+@login_required(login_url="/admin")
+def restricted(request):
+    return render(request, "home/restricted.html", {})
